275431ce563540128808fdef42c4cbb0a844dbe8
Williams Acuña conyas <WillAcon@users.noreply.github.com>
2015-12-29 15:23:14 -0500

    Meta

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -9,9 +9,6 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <head>
   <meta charset="utf-8">
-  <?php if ($mobile_friendly): ?>    
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php endif; ?>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
